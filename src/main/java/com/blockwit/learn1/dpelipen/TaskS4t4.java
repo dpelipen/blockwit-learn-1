42e716a17d24f2e087ebb3cac2c10954ad956665
Dmitriy Pelipenko <dvgushnik@gmail.com>
2022-01-17 02:19:50 +0300

    Task S4t4 completed

---
+++ src/main/java/com/blockwit/learn1/dpelipen/TaskS4t4.java
@@ -0,0 +1,22 @@
+package com.blockwit.learn1.dpelipen;
+
+import java.util.Scanner;
+
+public class TaskS4t4 {
+    public static void main(String[] args) {
+        int[] temp = new int[3];
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите температуру за последние три дня:");
+        int sum = 0;
+        int degree = 0;
+        for (int i = 0; i < temp.length; i++) {
+            temp[i] = in.nextInt();
+            sum += temp[i];
+            degree = sum / 3;
+
+        }
+
+        System.out.println("Средняя температура за три дня " + degree +" градусов");
+
+    }
+}
